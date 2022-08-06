d2632ccf9d14acb765343f7fb60f7ec4c655a713
Hwan-seok <tttkhs@naver.com>
2022-08-07 01:58:44 +0900

    docs: Add simple usage to README

--- test/toggleable_test.dart
+++ test/toggleable_test.dart
@@ -1,1 +1,19 @@
-void main() {}
+import 'package:toggleable/toggleable.dart';
+
+void main() {
+  final mySwitch = ToggleableState();
+
+  mySwitch.on();
+  mySwitch.isOn; // true
+  mySwitch.isEnabled; // true
+
+  mySwitch.off();
+  mySwitch.isOff; // true
+  mySwitch.isDisabled; // true
+
+  mySwitch.toggle();
+
+  int someOtherState = 1;
+
+  mySwitch.addListener(turnOnCallback: () => someOtherState++);
+}
