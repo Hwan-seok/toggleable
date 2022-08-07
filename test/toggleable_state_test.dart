e64beaeb7e398bd5bec24c808e37245e47fe6672
Hwan-seok <tttkhs@naver.com>
2022-08-08 01:35:18 +0900

    test: Add lib tests

---
+++ test/toggleable_state_test.dart
@@ -0,0 +1,97 @@
+import 'dart:async';
+
+import 'package:test/test.dart';
+import 'package:toggleable/toggleable.dart';
+
+void main() {
+  group("ToggleableState", () {
+    test("Default initial state is off", () {
+      final toggleableState = ToggleableState();
+
+      expect(toggleableState.state, Toggleable.off);
+    });
+
+    test('.toggle() makes toggled state', () {
+      final toggleableState = ToggleableState(initialState: Toggleable.on);
+
+      toggleableState.toggle();
+
+      expect(toggleableState.state, Toggleable.off);
+    });
+
+    test('notifyUpdate should called immediately after state changed if registered', () {
+      final toggleableState = ToggleableState();
+      bool isUpdated = false;
+      toggleableState.registerNotifyUpdate(() => isUpdated = true);
+
+      toggleableState.toggle();
+
+      expect(isUpdated, isTrue);
+    });
+  });
+
+  group('ToggleableState listeners', () {
+    test('should be called sequentially', () {
+      final toggleableState = ToggleableState();
+      final streamController = StreamController<int>();
+
+      toggleableState.addListener(turnOnCallback: () => streamController.add(1));
+      toggleableState.addListener(turnOnCallback: () => streamController.add(2));
+      toggleableState.addListener(turnOnCallback: () => streamController.add(3));
+      toggleableState.addListener(turnOnCallback: () => streamController.add(4));
+
+      toggleableState.on();
+
+      expect(streamController.stream, emitsInOrder([1, 2, 3, 4]));
+    });
+
+    test('should be called sequentially with async callback too', () {
+      final toggleableState = ToggleableState();
+
+      final streamController = StreamController<int>();
+
+      toggleableState.addListener(
+        turnOnCallback: () => Future.delayed(Duration(milliseconds: 400)).then(
+          (value) => streamController.add(1),
+        ),
+      );
+      toggleableState.addListener(
+        turnOnCallback: () => Future.delayed(Duration(milliseconds: 300)).then(
+          (value) => streamController.add(2),
+        ),
+      );
+      toggleableState.addListener(
+        turnOnCallback: () => Future.delayed(Duration(milliseconds: 200)).then(
+          (value) => streamController.add(3),
+        ),
+      );
+      toggleableState.addListener(
+        turnOnCallback: () => Future.delayed(Duration(milliseconds: 100)).then(
+          (value) => streamController.add(4),
+        ),
+      );
+
+      toggleableState.on();
+
+      expect(streamController.stream, emitsInOrder([1, 2, 3, 4]));
+    });
+
+    test('callback should executed only once if listenerDelay provided', () {
+      final toggleableState = ToggleableState(listenerDelay: Duration(seconds: 1));
+      final streamController = StreamController<int>();
+
+      toggleableState.addListener(
+        turnOnCallback: () => streamController.add(1),
+        turnOffCallback: () => streamController.add(1),
+      );
+
+      toggleableState.toggle();
+      toggleableState.toggle();
+      toggleableState.toggle();
+      toggleableState.toggle();
+      toggleableState.toggle();
+
+      expect(streamController.stream, emitsInOrder([1]));
+    });
+  });
+}
--- test/toggleable_test.dart
+++ test/toggleable_test.dart
@@ -1,19 +1,69 @@
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
 import 'package:toggleable/toggleable.dart';
 
 void main() {
-  final mySwitch = ToggleableState();
+  group('Toggleable', () {
+    test('.toggled() should return toggled value', () {
+      Toggleable toggleable1 = Toggleable.off;
+      expect(toggleable1.toggled(), Toggleable.on);
 
-  mySwitch.on();
-  mySwitch.isOn; // true
-  mySwitch.isEnabled; // true
+      Toggleable toggleable2 = Toggleable.on;
+      expect(toggleable2.toggled(), Toggleable.off);
+    });
 
-  mySwitch.off();
-  mySwitch.isOff; // true
-  mySwitch.isDisabled; // true
+    test('.toggled() does not changes the original value', () {
+      Toggleable toggleable = Toggleable.off;
 
-  mySwitch.toggle();
+      toggleable.toggled();
 
-  int someOtherState = 1;
+      expect(toggleable, Toggleable.off);
+    });
 
-  mySwitch.addListener(turnOnCallback: () => someOtherState++);
+    test('.isOn and .isEnabled returns whether the value is on', () {
+      Toggleable toggleable = Toggleable.on;
+
+      expect(toggleable.isOn, isTrue);
+      expect(toggleable.isEnabled, isTrue);
+      expect(toggleable.toggled().isOn, isFalse);
+    });
+
+    test('.isOff and .isDisabled returns whether the value is off', () {
+      Toggleable toggleable = Toggleable.off;
+
+      expect(toggleable.isOff, isTrue);
+      expect(toggleable.isDisabled, isTrue);
+      expect(toggleable.toggled().isOff, isFalse);
+    });
+
+    test('.when() executes the function by current state - (off should be called)', () {
+      Toggleable toggleableOff = Toggleable.off;
+
+      bool didOnFunctionCalled = false;
+      bool didOffFunctionCalled = false;
+
+      toggleableOff.when(
+        on: () => didOnFunctionCalled = true,
+        off: () => didOffFunctionCalled = true,
+      );
+
+      expect(didOnFunctionCalled, isFalse);
+      expect(didOffFunctionCalled, isTrue);
+    });
+
+    test('.when() executes the function by current state - (on should be called)', () {
+      Toggleable toggleableOff = Toggleable.on;
+
+      bool didOnFunctionCalled = false;
+      bool didOffFunctionCalled = false;
+
+      toggleableOff.when(
+        on: () => didOnFunctionCalled = true,
+        off: () => didOffFunctionCalled = true,
+      );
+
+      expect(didOnFunctionCalled, isTrue);
+      expect(didOffFunctionCalled, isFalse);
+    });
+  });
 }
